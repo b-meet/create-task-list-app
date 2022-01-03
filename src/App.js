df3ea301884fd77808fc4fee48d179b4becbc2a7
Meet <meetbhalodiya1030@gmail.com>
2022-01-04 00:02:01 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { FiEdit, FiTrash2 } from "react-icons/fi";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [input, setInput] = useState("");
+	const [list, setList] = useState([]);
+	const [message, setMessage] = useState("");
+
+	const handleChange = (e) => {
+		setInput(e.target.value);
+	};
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		if (input) {
+			setList([...list, input]);
+			setInput("");
+		} else {
+			setMessage("Please Add Some Chore");
+		}
+	};
+
+	return (
+		<section className='content-container'>
+			<h2 className='heading'>Task List</h2>
+			<form className='form-container'>
+				<input
+					type='text'
+					name='item'
+					id='item'
+					value={input}
+					onChange={handleChange}
+					placeholder='Type your chore'
+				/>
+				<button onClick={handleSubmit} type='submit' className='btn'>
+					Add
+				</button>
+			</form>
+			{list.map((item) => {
+				return (
+					<ul className='list-container'>
+						<li className='list-data'>
+							<p>{item}</p>
+							<div>
+								<button type='button' className='edit-btn'>
+									<FiEdit />
+								</button>
+								<button type='button' className='delete-btn'>
+									<FiTrash2 />
+								</button>
+							</div>
+						</li>
+					</ul>
+				);
+			})}
+		</section>
+	);
 }
 
 export default App;
